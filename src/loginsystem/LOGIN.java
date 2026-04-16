d15d2b15b938f1934628a55a162cbfc2ed6df106
zuchinni01 <20240888@nbsc.edu.ph>
2026-04-16 01:40:14 -0500

    ADDED sPACES

--- src/loginsystem/LOGIN.java
+++ src/loginsystem/LOGIN.java
@@ -11,6 +11,9 @@ import javax.swing.JOptionPane;
 
 public class LOGIN extends javax.swing.JFrame {
     
+    
+    
+    
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(LOGIN.class.getName());
 
     /**
